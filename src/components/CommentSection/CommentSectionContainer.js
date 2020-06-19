e6f3adb05932dcae0a1fffed092c33361e56473b
Vincent Sanders <vincesanders@hotmail.com>
2020-06-18 19:49:23 -0500

    Completed MVP and stretch.

--- src/components/CommentSection/CommentSectionContainer.js
+++ src/components/CommentSection/CommentSectionContainer.js
@@ -4,13 +4,31 @@ import CommentInput from "./CommentInput";
 import Comment from "./Comment";
 import "./Comment.css";
 
-const CommentSection = props => {
+const CommentSection = ({ imageUrl, comments, setComments }) => {
   // Add state for the comments
+  // const [comments, setComments] = useState(props.comments);
+
+  const [comment, setComment] = useState('')
+
+  const changeComment = e => {
+    setComment(e.target.value);
+  }
+
+  const submitComment = e => {
+    e.preventDefault();
+    const newComment = {
+      username: 'krayziesensei',
+      text: comment
+    }
+    setComments([...comments, newComment]);
+    setComment('');
+  }
 
   return (
     <div>
       {/* map through the comments data and return the Comment component */}
-      <CommentInput />
+      {comments.map((comment, i) => <Comment key={i} comment={comment} />)}
+      <CommentInput submitComment={submitComment} changeComment={changeComment} comment={comment} />
     </div>
   );
 };
--- src/components/PostsContainer/PostsPage.js
+++ src/components/PostsContainer/PostsPage.js
@@ -4,12 +4,14 @@ import React from "react";
 import Post from "./Post";
 import "./Posts.css";
 // import data 
+import data from '../../dummy-data';
 
 const PostsPage = () => {
   // set up state for your data
   return (
     <div className="posts-container-wrapper">
       {/* map through data here to return a Post and pass data as props to Post */}
+      {data.map((post, i) => <Post key={i} post={post} />)}
     </div>
   );
 };
